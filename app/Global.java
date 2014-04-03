27a9d3816a009155fd15967844a99f9972cbf793
Gabriel Cypriano <gabrielcs@gmail.com>
2014-04-03 13:14:54 -0300

    changing to Heroku's nomenclature

--- app/Global.java
+++ app/Global.java
@@ -2,13 +2,18 @@ import play.GlobalSettings;
 import play.Logger;
 
 public class Global extends GlobalSettings {
-	
+
+	private static final String WEB_PROCESS_TYPE = "web";
+	private static final String SCHEDULED_TICK_PROCESS_TYPE = "scheduledtick";
+
 	private boolean isWebApplication(play.Application app) {
-		return app.configuration().getBoolean("application.web");
+		return app.configuration().getString("process.type")
+				.equals(WEB_PROCESS_TYPE);
 	}
-	
+
 	private boolean isTickJob(play.Application app) {
-		return app.configuration().getBoolean("application.scheduledtick");
+		return app.configuration().getString("process.type")
+				.equals(SCHEDULED_TICK_PROCESS_TYPE);
 	}
 
 	@Override
@@ -16,7 +21,7 @@ public class Global extends GlobalSettings {
 		if (isWebApplication(app)) {
 			Logger.debug("Global.onStart() called from the Web Application");
 		}
-		
+
 		if (isTickJob(app)) {
 			Logger.debug("Global.onStart() called from the TickJob");
 		}
